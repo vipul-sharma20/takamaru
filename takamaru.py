40d18e694233d244e1698ccb5c8ba647faf8c1e0
Vipul <vipul.sharma20@gmail.com>
2017-05-16 00:00:50 +0530

    fix search call

--- takamaru.py
+++ takamaru.py
@@ -18,7 +18,8 @@ class Reddit(object):
         self.reddit = praw.Reddit(client_id=client_id,
                                   client_secret=client_secret,
                                   username=username,
-                                  password=password)
+                                  password=password,
+                                  user_agent='script by /u/{}'.format(username))
 
     def search(self, hot=False, *args, **kwargs):
         query = lambda q: '+'.join(q)
@@ -30,7 +31,7 @@ class Reddit(object):
                 results.append(submissions)
 
         else:
-            sub = self.reddit.subreddits(query(SUBREDDITS))
+            sub = self.reddit.subreddit(query(SUBREDDITS))
 
             for q in QUERIES:
                 submissions = sub.search(query=query(q),
@@ -41,8 +42,13 @@ class Reddit(object):
         return results
 
 
-if __name__ == '__name__':
+if __name__ == '__main__':
     r_instance = Reddit()
     posts_no_hot = r_instance.search()
     posts_hot = r_instance.search(hot=True)
-
+    for pnh in posts_no_hot:
+        for p in pnh:
+            print(p.title)
+    for ph in posts_hot:
+        for p in ph:
+            print(p.title)
